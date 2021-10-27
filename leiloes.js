9250cff162a7200e8e917469e0d7d783e57ecb7c
tiagombp <tiagombp@gmail.com>
2021-10-27 08:42:31 -0300

    popula valores

--- leiloes.js
+++ leiloes.js
@@ -28,7 +28,9 @@ const s = {
 
               })
               .then(contents => {
-                s.data.raw = contents;
+
+                s.control.after_init(contents);
+
               })
 
         }
@@ -39,16 +41,51 @@ const s = {
 
         sum : (array) => array.reduce(
             (va_acumulado, va_atual) => va_acumulado + va_atual
-        )
+        ),
+
+        sum_column : (array, column) => s.utils.sum( array.map(d => d[column]) ),
+
+        count : (array) => array.length,
+
+        format : (number) => number.toFixed(2).replace('.', ','),
+
+        populate_data_field : (ref) => {
+
+            // ref vai ser tanto a referencia para selecionar o campo, quando a referencia à chave do objeto s.data.summary
+
+            let el = document.querySelector('[data-vartext-' + ref + ']');
+            
+            let value = s.data.summary[ref];
+
+            // special treatments
+            if (ref == 'va_financeiro_total') { value = `R$ ${s.utils.format(value/1e12)} trilhões`}
+
+            el.innerText = value;
+
+        }
 
     },
 
-    init : function() {
+    control : {
 
-        s.data.read();
+        init : () => s.data.read(),
+
+        after_init : (data) => {
+
+            s.data.raw = data;
+
+            s.data.summary.qde_leiloes = s.utils.count(data);
+            s.data.summary.va_financeiro_total = s.utils.sum_column(data, 'VA_FINANCEIRO_ACEITO');
+
+            // popula textos
+            const summary_keys = Object.keys(s.data.summary);
+            summary_keys.forEach(key => s.utils.populate_data_field(key));
+
+
+        }
 
     }
 
 }
 
-s.init();
+s.control.init();
